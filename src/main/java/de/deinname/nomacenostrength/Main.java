37c5365f27485b650458e505535ee7a1f0b17d1f
Felixwrn <104013013+Felixwrn@users.noreply.github.com>
2026-03-24 21:41:50 +0100

    Update Main.java

--- src/main/java/de/deinname/nomacenostrength/Main.java
+++ src/main/java/de/deinname/nomacenostrength/Main.java
@@ -4,8 +4,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+    public static boolean maceEnabled = false; // Default: Maces deaktiviert
+
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(new ListenerClass(), this);
+        this.getCommand("togglemace").setExecutor(new ToggleMaceCommand());
     }
 }
