242165ed66ed40b2285d9a23b3766abbb89c9333
Felixwrn <104013013+Felixwrn@users.noreply.github.com>
2026-03-28 22:41:56 +0100

    Update ListenerClass.java

--- src/main/java/de/deinname/nomacenostrength/ListenerClass.java
+++ src/main/java/de/deinname/nomacenostrength/ListenerClass.java
@@ -6,10 +6,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.*;
-import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent; // ✅ DAS HINZUFÜGEN
 import org.bukkit.event.player.PlayerInteractEvent;
-
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
